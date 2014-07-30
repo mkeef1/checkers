50b11e320b73018b915d994887933fc1b5821e01
Chyld Medford <chyld.medford@gmail.com>
2014-07-30 16:25:17 -0500

    added EJS files

--- app/index.js
+++ app/index.js
@@ -11,7 +11,41 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
-  res.render('home.ejs');
+  res.render('home');
+});
+
+app.get('/sum', function(req, res){
+  res.render('sum');
+});
+
+app.get('/add/:a/:b/:c/:d', function(req, res){
+  req.params.a *= 1;
+  req.params.b *= 1;
+  req.params.c *= 1;
+  req.params.d *= 1;
+
+  req.params.e = req.query.fontsize;
+  req.params.f = req.query.color;
+  req.params.g = req.query.border;
+  req.params.h = req.query.bordercolor;
+  req.params.i = req.query.borderwidth;
+
+  res.render('sum', req.params);
+});
+
+app.get('/sum_list/:nums', function(req, res){
+  var nums = req.params.nums.split(',');
+
+  nums = nums.map(function(n){
+    return n * 1;
+});
+
+  var sum = 0;
+  for(var i = 0; i < nums.length; i++){
+    sum +=nums[i];
+  }
+
+  res.render('sum_list', {nums:nums, sum:sum, even:req.query.even, odd:req.query.odd});
 });
 
 var port = process.env.PORT;
